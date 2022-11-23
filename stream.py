5f94ac695ac8631f03ab92673cf0f1c7b17b3373
Fergus Gallagher <fergus@gooses.co.uk>
2022-11-23 17:15:59 +0000

    WIP

--- stream.py
+++ stream.py
@@ -1,80 +1,12 @@
+import json
 import os
 import sys
 import time
-import redis as R
-from consumer import Consumer
-from utils import (
-    dot,
-    filter_old_message,
-    gen_group_name,
-    has_results,
-    purge,
-    rand_t,
-    redis,
-    btos,
-)
+
+from utils import rand_t, redis
 
 # Probably user's "bank"
-STREAM = os.getenv("STREAM", "mystream")
-
-
-def reset_stream():
-    redis.xtrim(name=STREAM, maxlen=0, approximate=False)
-
-
-def default_on_message(*args, **kwargs):
-    # do something with the message here
-    def default(*args):
-        (id, msg) = args
-        print(f"Got message", btos(id), msg)
-
-    return default(*args)
-
-
-def default_filter(*args, **kwargs) -> bool:
-    return True
-
-
-def handle_messages(
-    messages: list,
-    group_name: str,
-    last_id="0-0",
-    filter_fn=None,
-    on_message=None,
-) -> str:
-    if filter_fn is None:
-        filter_fn = default_filter
-    if on_message is None:
-        on_message = default_on_message
-    ids = []
-    try:
-        for message in messages:
-            (id, fields) = message
-            try:
-                if filter_fn(message):
-                    on_message(id, fields)
-                else:
-                    print(f"Skipping message: {id}")
-            except Exception as e:
-                print(e)
-            finally:
-                last_id = id
-                ids.append(id)
-    except Exception as e:
-        print(e)
-    finally:
-        redis.xack(STREAM, group_name, *ids)
-        return last_id
-
-
-# needed?
-def create_group(group_name: str):
-    try:
-        redis.xgroup_create(name=STREAM, groupname=group_name, id="$", mkstream=True)
-    except R.exceptions.ResponseError as e:
-        if str(e) == "BUSYGROUP Consumer Group name already exists":
-            return
-        raise
+STREAM = os.getenv("STREAM", "mypubsub")
 
 
 def producer(p: int):
@@ -85,12 +17,9 @@ def producer(p: int):
         sleep_t = 1
         n = 10
     for i in range(n):
-        resp = redis.xadd(
-            name=STREAM,
-            fields={"seq_id": i, "producer": p},  # any random JSON
-            id="*",  # auto-generate id
-            maxlen=100,  # keep last 100 messages (approx, but always at least 100)
-            approximate=True,  # don't worry about exact maxlen - much faster this way
+        resp = redis.publish(
+            channel=STREAM,
+            message=json.dumps({"time": time.time()}),  # any random JSON
         )
         if p == 1:
             sleep_t = rand_t()
@@ -98,57 +27,26 @@ def producer(p: int):
         time.sleep(sleep_t)
 
 
-def simple_consumer(group_name: str, last_id: str = ">", quiet=True):
-    with Consumer(
-        group_name=group_name, redis=redis, stream=STREAM, quiet=quiet
-    ) as consumer:
-        results = consumer.xreadgroup(
-            groupname=group_name,
-            consumername=consumer.name,
-            streams={STREAM: last_id},
-            count=None,  # get all messages
-            block=500,
-            noack=False,
-        )
+def simple_consumer():
+    pubsub = redis.pubsub()
+    pubsub.subscribe(STREAM)
 
-        if not has_results(results):
-            dot()
-            return
-        print()
-        # only 1 stream
-        (_stream, messages) = results[0]
-
-        handle_messages(
-            messages=messages,
-            group_name=group_name,
-            filter_fn=filter_old_message(),
-            on_message=default_on_message,
-        )
+    while True:
+        print("subscribing")
+        for message in pubsub.listen():
+            if message["type"] == "message":
+                m = json.loads(message["data"])
+                now = time.time()
+                t = m["time"]
+                print(m, now - t)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
     opt = sys.argv[1]
-    if opt == "producer1":
+    if opt == "producer":
         producer(1)
-    elif opt == "producer2":
-        producer(2)
     elif opt == "C":
-        group_name = gen_group_name(opt)
-        simple_consumer(group_name, quiet=False)
-    elif opt == "D" or opt == "E":
-        group_name = gen_group_name(opt)
-        while True:
-            simple_consumer(group_name, quiet=True)
-    elif opt == "consumers":
-        purge(delete=False, stream=STREAM)
-    elif opt == "purge":
-        purge(stream=STREAM)
-        # Need to call twice to delete groups with no consumers
-        purge(quiet=True, stream=STREAM)
-    elif opt == "purge-all":
-        purge(delta=0, stream=STREAM)
-        purge(delta=0, quiet=True, stream=STREAM)
-    elif opt == "reset":
-        reset_stream()
+        simple_consumer()
     else:
         print("unknown option", opt)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-import time
 from random import random
 from typing import Tuple
+
 from dotenv import dotenv_values
 from redis import Redis
 
@@ -11,16 +11,6 @@ def btos(s: bytes) -> str:
     return s.decode("utf-8")
 
 
-def parse_id(id: bytes) -> Tuple[int, int]:
-    s = id.decode("utf-8")
-    (ts, seq) = s.split("-")
-    return (int(ts), int(seq))
-
-
-def gen_group_name(group_id: str) -> str:
-    return f"group-{group_id}"
-
-
 def rand_t():
     return random() * 0.1
 
@@ -29,53 +19,9 @@ def dot():
     print(".", end="", flush=True)
 
 
-def has_results(results: list) -> bool:
-    return not (len(results) == 0 or len(results[0][1]) == 0)
-
-
-def ack_all(messages: list, group_name: str, stream=None):
-    ids = [id for (id, _fields) in messages]
-    if len(ids):
-        redis.xack(stream, group_name, *ids)
-
-
-def purge(delete=True, delta=600_000, quiet=False, stream=None):
-    """
-    Delete idle consumers and groups with no consumers.
-
-    This func should be a dud as all consumers are ephemeral and should be deleted when they exit.
-    """
-    for g in redis.xinfo_groups(stream):
-        if g["consumers"] == 0:
-            continue
-        else:
-            print("warning: group has consumers")
-            print(g)
-        grp_name = g["name"]
-        for c in redis.xinfo_consumers(stream, grp_name):
-            if not quiet:
-                print("\t", c)
-            # Hmmm idle doesn't seem to reset as advertised.
-            if delete and c["idle"] > delta:
-                print("deleting consumer", c["name"])
-                redis.xgroup_delconsumer(stream, grp_name, c["name"])
-
-
 redis = Redis(
     host=config.get("host", "localhost"),
     port=int(config.get("port", 6379)),
     password=config.get("password", None),
     db=int(config.get("db", 0)),
 )
-
-
-def filter_old_message(skip_t: int = 10_000) -> bool:
-    def filter_message(message: Tuple[str, any], skip_t: int) -> bool:
-        (id, _fields) = message
-        (ts, _seq) = parse_id(id)
-
-        now = int(time.time() * 1000)
-        # Ignore messages older than skip_t ms
-        return now - ts < skip_t
-
-    return lambda message: filter_message(message, skip_t)
